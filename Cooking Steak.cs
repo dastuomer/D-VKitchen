067b3524661fa61dcda6ac5b7c9151233984c8d6
Dastu <112965095+dastuomer@users.noreply.github.com>
2024-04-11 19:53:37 -0600

    commenting CookingSteak.cs

--- Cooking Steak.cs
+++ Cooking Steak.cs
@@ -6,29 +6,31 @@ using UnityEngine;
 
 public class Steak : MonoBehaviour
 {
-    AudioManager audioManager;
+    AudioManager audioManager; // reference to the AudioManager script
 
-    public ParticleSystem cookingSteak;
+    public ParticleSystem cookingSteak; // reference to the particle system for cooking steak
 
-    public GameObject[] cookedSteak;
+    public GameObject[] cookedSteak; // array of cooked steak peices
 
     private void Awake()
-
     {
-
+        // finding and geting a reference to the AudioManager
+        // references the AudioManager componenet that is attached to a GameObject tagged as "Audio"
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
-
     }
+
     public void CookSteak()
     {
+        // play the food sizzling sound effect using the AudioManager
         audioManager.PlaySFX(audioManager.FoodSizzling);
 
+        // disable the raw steak GameObject
         gameObject.SetActive(false);
 
-
+        // loop throughthe cookedSteak array
         foreach (GameObject piece in cookedSteak)
         {
-
+            // activate each cooked steak piece GameObject
             piece.SetActive(true);
         }
     }
