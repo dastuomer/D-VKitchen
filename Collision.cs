1174c27847510f96541cce366d0f14e744927316
Venessa <146301351+Venessawarefa@users.noreply.github.com>
2024-04-11 19:28:38 -0600

    Collision.cs

--- Collision.cs
+++ Collision.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class collision : MonoBehaviour
 {
-    
+    //When a collision occurs this method is called.
     void OnCollisionEnter(Collision collision)
     {
-        // Check if the object collided with has the tag "Onion"
+        // Check if the object collided with, has the tag "Onion".
         if (collision.gameObject.CompareTag("Onion"))
         {
-            
+
+            //If the collided object has the tag "Onion", call the chop method.
             collision.gameObject.GetComponent<Onion>().Chop();
         }
     }
