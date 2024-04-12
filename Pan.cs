7d6ab09f08cb4fea398a4106a294cbcdc49a9ac9
Dastu <112965095+dastuomer@users.noreply.github.com>
2024-04-11 19:52:00 -0600

    Comments in Pan.cs

--- Pan.cs
+++ Pan.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class pan : MonoBehaviour
 {
-
+    // this is called when a collision occurs with the pan 
     void OnCollisionEnter(Collision collision)
     {
+        // checking if its the steak
         if (collision.gameObject.CompareTag("Steak"))
         {
-
+            // if it is the steak
+            // call the Cook.Steak method (which is what changes the raw steak to the cooked steak) 
             collision.gameObject.GetComponent<Steak>().CookSteak();
         }
     }
