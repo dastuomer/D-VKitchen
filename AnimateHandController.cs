36fc37dd2d40c8a83a8e30c5fc2c2dc58bff980f
Dastu <112965095+dastuomer@users.noreply.github.com>
2024-04-11 19:54:19 -0600

    Commenting AnimateHandController.cs

--- AnimateHandController.cs
+++ AnimateHandController.cs
@@ -1,37 +1,36 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Animator))] //attaches the animator to the gameobject
+[RequireComponent(typeof(Animator))] // the animator component to be attached to the GameObject
 public class AnimateHandController : MonoBehaviour
 {
-    public InputActionReference gripInputActionReference;
-    public InputActionReference triggerInputActionReference;
+    public InputActionReference gripInputActionReference; // reference to grip input action
+    public InputActionReference triggerInputActionReference; // reference to trigger input action
 
-    private Animator _handAnimator;
-    private float _gripValue;
-    private float _triggerValue;
+    private Animator _handAnimator; // reference to the animator component
+    private float _gripValue; // storing the value of grip input
+    private float _triggerValue; // storing the value of trigger input
 
-    private void start()
+    private void Start() // called when the GameObject is first enabled
     {
-        _handAnimator = GetComponent<Animator>(); // gets the hand animator
+        _handAnimator = GetComponent<Animator>(); // getting the animator component attached to the GameObject
     }
 
-    private void Update() //updates every frame
+    private void Update() // called for every frame
     {
-        AnimateGrip();
-        AnimateTrigger();
+        AnimateGrip(); // animate grip based on input
+        AnimateTrigger(); // animate trigger based on input
     }
 
-    private void AnimateGrip() // assign the grip value
+    private void AnimateGrip() // animate grip based on input value
     {
-        _gripValue = gripInputActionReference.action.ReadValue<float>();
-        _handAnimator.SetFloat("Grip", _gripValue);
+        _gripValue = gripInputActionReference.action.ReadValue<float>(); // reading the value of the grip input
+        _handAnimator.SetFloat("Grip", _gripValue); // setting the "Grip" parameter in the animator to the grip value
     }
 
-    private void AnimateTrigger()// assign the trigger value
+    private void AnimateTrigger() // animate trigger based on input value
     {
-        _triggerValue = triggerInputActionReference.action.ReadValue<float>();
-        _handAnimator.SetFloat("Trigger", _triggerValue);
+        _triggerValue = triggerInputActionReference.action.ReadValue<float>(); // reading the value of the trigger input
+        _handAnimator.SetFloat("Trigger", _triggerValue); // setting the "Trigger" parameter in the Animator to the trigger value
     }
-
 }
